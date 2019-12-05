2bdc8ce52718734d388fe672037b6335900750af
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-12-05 19:03:30 +0900

    refactor(header): ヘッダー周り最適化

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {
-    path: 'header',
-    loadChildren: () => import('./header/header.module').then(m => m.HeaderModule)
-  }
 ];
 
 @NgModule({

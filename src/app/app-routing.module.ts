9f54097d6328e9fcd433ea88fb415ba820b11cb3
moriyamasaaaki <moriya-7071@outlook.com>
2019-12-09 02:49:39 +0900

    feat(detail): 詳細ページ作成 #8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
     path: 'top',
     loadChildren: () => import('./top/top.module').then(m => m.TopModule)
   },
+
+  {
+    path: 'detail',
+    loadChildren: () => import('./detail/detail.module').then(m => m.DetailModule)
+  }
 ];
 
 @NgModule({

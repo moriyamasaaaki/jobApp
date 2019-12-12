91e14fbcbbc67e2c587c3e454b4c8845d88b7a7c
moriyamasaaaki <moriya-7071@outlook.com>
2019-12-13 03:36:01 +0900

    feat(profile): フォームvalidate追加 #13

--- src/app/profile/profile/profile.component.ts
+++ src/app/profile/profile/profile.component.ts
@@ -8,6 +8,28 @@ import { FormBuilder, Validators, FormControl } from '@angular/forms';
 })
 export class ProfileComponent implements OnInit {
 
+years = [
+  1960,
+  1961,
+  1962,
+  1963,
+  1964,
+  1965
+];
+
+months = new Array(13).fill(null);
+days = new Array(31).fill(null);
+
+
+schools = [
+  '中学','高校','専門','大学','大学院'
+];
+
+states = [
+  '卒業','在学中','中退'
+];
+
+
   form = this.fb.group({
     myName: ['', [
       Validators.required,
@@ -16,6 +38,55 @@ export class ProfileComponent implements OnInit {
     address: ['', [
       Validators.required,
     ]],
+
+    year: ['', [
+      Validators.required,
+    ]],
+
+    month: ['', [
+    Validators.required,
+    ]],
+
+    day: ['', [
+      Validators.required,
+    ]],
+
+    // gender: ['', [
+    //   Validators.required,
+    // ]],
+
+    // gender: this.fb.group({
+    //   men: [false],
+    //   women: [false],
+    // })
+
+    email: ['', [
+      Validators.required,
+      Validators.email,
+    ]],
+
+    telephone: ['', [
+      Validators.required,
+      Validators.pattern(/^0\d{9,10}$/),
+    ]],
+
+    school: ['', [
+      Validators.required,
+    ]],
+
+    state: ['', [
+      Validators.required,
+    ]],
+
+    possibleDay: ['', [
+      Validators.required,
+    ]],
+
+
+
+
+
+
 
   });
 
@@ -26,6 +97,50 @@ export class ProfileComponent implements OnInit {
   get addressControl() {
     return this.form.get('address') as FormControl;
   }
+
+    get yearControl() {
+    return this.form.get('year') as FormControl;
+  }
+
+    get monthControl() {
+    return this.form.get('month') as FormControl;
+  }
+
+    get dayControl() {
+    return this.form.get('day') as FormControl;
+  }
+
+  //   get genderControl() {
+  //   return this.form.get('gender') as FormControl;
+  // }
+
+    get emailControl() {
+    return this.form.get('email') as FormControl;
+  }
+
+    get telephoneControl() {
+    return this.form.get('telephone') as FormControl;
+  }
+
+    get schoolControl() {
+    return this.form.get('school') as FormControl;
+  }
+
+    get stateControl() {
+    return this.form.get('state') as FormControl;
+  }
+
+    get possibleDayControl() {
+    return this.form.get('possibleDay') as FormControl;
+  }
+
+
+
+
+
+
+
+
 
 
   constructor(

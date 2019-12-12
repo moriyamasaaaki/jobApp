ec9c4f6027b8610c55712c955ae3e6182ea0b45a
moriyamasaaaki <moriya-7071@outlook.com>
2019-12-13 04:43:46 +0900

    feat(register-form): 企業側登録フォーム マークアップ #10

--- src/app/company-side/company-side.module.ts
+++ src/app/company-side/company-side.module.ts
@@ -4,12 +4,20 @@ import { CommonModule } from '@angular/common';
 import { CompanySideRoutingModule } from './company-side-routing.module';
 import { RegisterFormComponent } from './register-form/register-form.component';
 
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [RegisterFormComponent],
   imports: [
     CommonModule,
-    CompanySideRoutingModule
+    CompanySideRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule
   ]
 })
-export class CompanySideModule { }
+export class CompanySideModule {}

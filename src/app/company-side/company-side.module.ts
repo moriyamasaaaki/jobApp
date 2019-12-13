32a2086059b74ad02214e530ba08e0c3f48a2f08
moriyamasaaaki <moriya-7071@outlook.com>
2019-12-13 14:17:53 +0900

    feat(register-form): 企業側登録フォームvalidate追加 #27

--- src/app/company-side/company-side.module.ts
+++ src/app/company-side/company-side.module.ts
@@ -4,20 +4,25 @@ import { CommonModule } from '@angular/common';
 import { CompanySideRoutingModule } from './company-side-routing.module';
 import { RegisterFormComponent } from './register-form/register-form.component';
 
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [RegisterFormComponent],
   imports: [
     CommonModule,
     CompanySideRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class CompanySideModule {}
--- src/app/company-side/register-form/register-form.component.ts
+++ src/app/company-side/register-form/register-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
@@ -7,9 +8,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterFormComponent implements OnInit {
 
-  constructor() { }
+  form = this.fb.group({
+    companyName: ['', [
+      Validators.required,
+    ]],
+
+    lastName: ['', [
+      Validators.required,
+      Validators.maxLength(30)
+    ]],
+
+    firstName: ['', [
+      Validators.required,
+      Validators.maxLength(30)
+    ]],
+
+    email: ['', [
+      Validators.required,
+      Validators.email
+    ]],
+
+    password: ['', [
+      Validators.required,
+      Validators.pattern(/^([a-zA-Z0-9]{8,})$/)
+    ]],
+
+    // terms: ['', [
+    //   Validators.required,
+    // ]],
+  });
+
+  hide = true;
+
+
+
+    get companyNameControl() {
+    return this.form.get('companyName') as FormControl;
+  }
+
+    get lastNameControl() {
+    return this.form.get('lastName') as FormControl;
+  }
+
+    get firstNameControl() {
+    return this.form.get('firstName') as FormControl;
+  }
+
+      get emailControl() {
+    return this.form.get('email') as FormControl;
+  }
+
+
+    get passwordControl() {
+    return this.form.get('password') as FormControl;
+  }
+
+  //   get termsControl() {
+  //   return this.form.get('terms') as FormControl;
+  // }
+
+
+
+
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
   }
+
+    submit() {
+    console.log(this.form.value);
+}
+
 
 }

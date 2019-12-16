61815b8769a11379ea25a708c4752dcfa2c9a3ff
moriyamasaaaki <moriya-7071@outlook.com>
2019-12-16 09:07:17 +0900

    feat(deatil): interface追加 #35

--- src/app/detail/detail/detail.component.ts
+++ src/app/detail/detail/detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from 'src/app/interfaces/article';
 
 @Component({
   selector: 'app-detail',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
+  // @Input() article: Article;
+
+  article: Article = {
+    title: 'タイトル',
+    tagu: 'タグ',
+    companyName: '会社名',
+    date: new Date(),
+    companyContent:  '会社概要が入ります。',
+    occupation: '職種が入ります。',
+    salary: '1000〜1200円',
+    welfare: 'あり',
+    workTime: '18:00〜22：00',
+    workPlace: '東京、新宿',
+    recruitmentImg: '/assets/images/job3.jpg'
+  };
 
   constructor() { }
 
---
+++ src/app/interfaces/article.ts
@@ -0,0 +1,13 @@
+export interface Article {
+  title: string;
+  tagu: string;
+  companyName: string;
+  date: Date;
+  companyContent: string;
+  occupation: string;
+  salary: string;
+  welfare: string;
+  workTime: string;
+  workPlace: string;
+  recruitmentImg: string;
+}

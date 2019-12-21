6542906499a1f63dffb60023bd3f2d45c2ca7343
moriyamasaaaki <moriya-7071@outlook.com>
2019-12-21 13:56:24 +0900

    refactor(joblist,keep): コード整理、削除 #47

--- src/app/keep/keep/keep.component.ts
+++ src/app/keep/keep/keep.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./keep.component.scss']
 })
 export class KeepComponent implements OnInit {
+  job = {
+    companyName: '株式会社サイバーエージェント',
+    title: 'Go経験のあるエンジニア募集',
+    img: 'assets/images/job3.jpg',
+    place: '渋谷駅',
+    occupation: 'フロント、サーバー、iOS、機械学習、データサイエンティスト',
+    salary: 2000,
+    workTime: '08:00～18:00'
+  };
 
-  constructor() { }
+  jobs = new Array(5).fill(this.job);
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }

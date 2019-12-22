76660fdd819c3c5028dc5d36f1d6b4b59d01cc9f
moriyamasaaaki <moriya-7071@outlook.com>
2019-12-23 08:24:48 +0900

    feat(create-review): レビュー投稿ページついか、Form作成 #39

--- src/app/create-review/create-review.module.ts
+++ src/app/create-review/create-review.module.ts
@@ -4,8 +4,25 @@ import { CommonModule } from '@angular/common';
 import { CreateReviewRoutingModule } from './create-review-routing.module';
 import { CreateReviewComponent } from './create-review/create-review.component';
 
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
   declarations: [CreateReviewComponent],
-  imports: [CommonModule, CreateReviewRoutingModule]
+  imports: [
+    CommonModule,
+    CreateReviewRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSliderModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule
+  ]
 })
 export class CreateReviewModule {}

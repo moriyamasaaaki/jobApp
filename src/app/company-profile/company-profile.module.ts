9fd8eacc8fe11c413bda37ee07a705cc977c8e33
moriyamasaaaki <moriya-7071@outlook.com>
2019-12-26 01:24:45 +0900

    style(companyProfile): style追加 #89

--- src/app/company-profile/company-profile.module.ts
+++ src/app/company-profile/company-profile.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { CompanyProfileRoutingModule } from './company-profile-routing.module';
 import { CompanyProfileComponent } from './company-profile/company-profile.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [CompanyProfileComponent],
-  imports: [CommonModule, CompanyProfileRoutingModule]
+  imports: [
+    CommonModule,
+    CompanyProfileRoutingModule,
+    MatButtonModule,
+    MatDialogModule
+  ]
 })
 export class CompanyProfileModule {}

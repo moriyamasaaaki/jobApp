3b4379145f92dea82471a70e074558f09a669a35
moriyamasaaaki <moriya-7071@outlook.com>
2019-12-25 17:32:02 +0900

    feat(mypage): Fire storeからの値の取り出し・表示 #86

--- src/app/mypage/mypage/mypage.component.ts
+++ src/app/mypage/mypage/mypage.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogComponent } from 'src/app/delete-dialog/delete-dialog.component';
+import { UserProfileService } from 'src/app/service/user-profile.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
+import { UserProfile } from 'src/app/interfaces/profile';
 
 @Component({
   selector: 'app-mypage',
@@ -8,7 +12,17 @@ import { DeleteDialogComponent } from 'src/app/delete-dialog/delete-dialog.compo
   styleUrls: ['./mypage.component.scss']
 })
 export class MypageComponent implements OnInit {
-  constructor(private dialog: MatDialog) {}
+  profile$: Observable<UserProfile> = this.UserProfileService.getUser(
+    this.AuthService.uid
+  );
+
+  constructor(
+    private dialog: MatDialog,
+    // tslint:disable-next-line: no-shadowed-variable
+    private UserProfileService: UserProfileService,
+    // tslint:disable-next-line: no-shadowed-variable
+    private AuthService: AuthService
+  ) {}
 
   openDeleteDialog() {
     this.dialog.open(DeleteDialogComponent);

6f90a02afc3be726af703dedd184f20fed52f3f5
moriyamasaaaki <moriya-7071@outlook.com>
2019-12-26 16:14:58 +0900

    feat(firestore/detail): firestoreデータ取得・表示 #90

--- src/app/detail/detail/detail.component.ts
+++ src/app/detail/detail/detail.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DetailJob } from 'src/app/interfaces/article';
+import { JobPostService } from 'src/app/service/job-post.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-detail',
@@ -7,24 +10,14 @@ import { DetailJob } from 'src/app/interfaces/article';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
-  article: DetailJob = {
-    title: 'バイトを探している方をサポート',
-    label: ['週２からOK', '未経験大歓迎'],
-    companyName: '株式会社Tokyo bite',
-    date: new Date(),
-    companyContent: '会社概要が入ります。会社概要が入ります。',
-    occupation: '職種が入ります。職種が入ります。',
-    salary: {
-      min: 1200,
-      max: 1300
-    },
-    welfare: 'あり',
-    workTime: '18:00〜22：00',
-    workPlace: '池袋',
-    recruitmentImg: '/assets/images/job3.jpg'
-  };
+  article$: Observable<DetailJob> = this.jobPostService.getJobPost(
+    this.authService.uid
+  );
 
-  constructor() {}
+  constructor(
+    private jobPostService: JobPostService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {}
 }
--- src/app/service/job-post.service.ts
+++ src/app/service/job-post.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { DamiJob } from '../interfaces/article';
+import { DetailJob } from '../interfaces/article';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobPostService {
-  constructor(private db: AngularFirestore, private snackBar: MatSnackBar) {}
+  constructor(
+    private db: AngularFirestore,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
 
-  createJobPost(article: DamiJob) {
+  createJobPost(article: DetailJob) {
     const id = this.db.createId();
     return this.db
       .doc(`JobPosts/${id}`)
@@ -18,6 +25,21 @@ export class JobPostService {
         this.snackBar.open('求人を作成しました', null, {
           duration: 2000
         });
+        this.router.navigateByUrl('/detail');
       });
+  }
+  getJobPost(jobId: string): Observable<DetailJob> {
+    return this.db
+      .collection<DetailJob>('JobPosts', ref => ref.where('jobId', '==', jobId))
+      .valueChanges()
+      .pipe(
+        map(JobPosts => {
+          if (JobPosts.length) {
+            return JobPosts[0];
+          } else {
+            return null;
+          }
+        })
+      );
   }
 }

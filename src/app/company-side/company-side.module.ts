205ce000ae2c0e4341c9b1ef280dfef2424700b3
moriyamasaaaki <moriya-7071@outlook.com>
2020-01-06 14:21:30 +0900

    feat(register-form): 利用規約追加、スタイル調整 #96

--- src/app/company-side/company-side.module.ts
+++ src/app/company-side/company-side.module.ts
@@ -5,14 +5,15 @@ import { CompanySideRoutingModule } from './company-side-routing.module';
 import { RegisterFormComponent } from './register-form/register-form.component';
 
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { TermsComponent } from '../legal/terms/terms.component';
 
 @NgModule({
-  declarations: [RegisterFormComponent],
+  declarations: [RegisterFormComponent, TermsComponent],
   imports: [
     CommonModule,
     CompanySideRoutingModule,

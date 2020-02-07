b2a58ec908288a34901dfa160983eb96e7e704dc
moriyamasaaaki <moriya-7071@outlook.com>
2020-02-07 15:40:42 +0900

    refacotr(app): 不要なコード削除 #34

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +8,5 @@ import { AuthService } from './services/auth.service';
 export class AppComponent {
   title = 'Tokyo bite';
 
-  constructor(private authService: AuthService) {}
-
-  login() {
-    this.authService.login();
-  }
+  constructor() {}
 }

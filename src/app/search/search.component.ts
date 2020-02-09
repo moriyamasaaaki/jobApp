a6b7c80c7c0893c8065a4df4409464d1ac97fe76
moriyamasaaaki <moriya-7071@outlook.com>
2020-02-09 14:33:10 +0900

    feat(search): algoliaUI #34

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormControl } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
-
+import { searchClient } from '../../environments/environment';
 
 @Component({
   selector: 'app-search',
@@ -9,44 +8,18 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
-form = this.fb.group({
-  keyword: ['', [
-    Validators.required,
-    Validators.maxLength(50)
-  ]],
-
-  place: ['', [
-    Validators.required,
-    Validators.maxLength(40)
-  ]],
-});
-
-
-get keywordControl() {
-  return this.form.get('keyword') as FormControl;
-}
-
-get placeControl() {
-  return this.form.get('place') as FormControl;
-}
+  config = {
+    indexName: 'JobPosts',
+    searchClient
+  };
 
   user$ = this.authService.afUser$;
 
-  constructor(
-    private fb: FormBuilder,
-    private authService: AuthService,
-  ) { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   companyLogin() {
     this.authService.companyLogin();
-  }
-
-
-  submit() {
-    console.log(this.form.value);
   }
 }

053a181cc8969ce5a49a18608d34847c9d6976c9
moriyamasaaaki <moriya-7071@outlook.com>
2020-02-11 04:50:19 +0900

    feat(search): algoliaスタイル追加 #34

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,10 +13,6 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {}
 
-  // login() {
-  //   this.authService.login();
-  // }
-
   logout() {
     this.authService.logout();
   }

0cb42911d9802ed4b7d9dbc445d1f2254d2a7a0c
moriyamasaaaki <moriya-7071@outlook.com>
2020-02-25 01:25:34 +0900

    refactor(service): 不要なコード削除 #144

--- src/app/services/liked.service.ts
+++ src/app/services/liked.service.ts
@@ -10,17 +10,17 @@ import { map, switchMap } from 'rxjs/operators';
 export class LikedService {
   constructor(private db: AngularFirestore) {}
 
-  //いいね追加
+  // いいね追加
   likedItem(id: string, userId: string): Promise<void> {
     return this.db.doc(`LikedUsers/${userId}/LikedItems/${id}`).set({ id });
   }
 
-  //いいねした人のユーザーID
+  // いいねした人のユーザーID
   likedUser(id: string, userId: string): Promise<void> {
     return this.db.doc(`likes/${id}/likedUsers/${userId}`).set({ userId });
   }
 
-  //いいね一覧取得
+  // いいね一覧取得
   getLikedJobs(userId: string) {
     return this.db
       .collection<Favorite>(`LikedUsers/${userId}/LikedItems`)
@@ -36,17 +36,17 @@ export class LikedService {
       );
   }
 
-  //いいねを削除
+  // いいねを削除
   deleteLikedJobs(userId: string, id: string): Promise<void> {
     return this.db.doc(`LikedUsers/${userId}/LikedItems/${id}`).delete();
   }
 
-  //いいねそたユーザー削除
+  // いいねそたユーザー削除
   deleteLikesUser(id: string, userId: string): Promise<void> {
     return this.db.doc(`likes/${id}/likedUsers/${userId}`).delete();
   }
 
-  //いいねしているかのチェック
+  // いいねしているかのチェック
   isLiked(id: string, userId: string): Observable<boolean> {
     return this.db
       .doc(`likes/${id}/likedUsers/${userId}`)

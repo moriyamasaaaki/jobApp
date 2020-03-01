dd21ff092d9945d4d4f54ac3353af58a896589be
moriyamasaaaki <moriya-7071@outlook.com>
2020-03-02 02:59:44 +0900

    feat(stripe.function): 決済function #32

--- functions/src/stripe.function.ts
+++ functions/src/stripe.function.ts
@@ -3,10 +3,12 @@ import * as admin from 'firebase-admin';
 
 const stripe = require('stripe')(functions.config().stripe.key);
 const db = admin.firestore();
-console.log(stripe);
-// 顧客作成関数
+
+const taxId = functions.config().stripe.tax_id;
+const planId = functions.config().stripe.plan_id;
+
+// ---------------------顧客作成関数----------------------------------
 export const createCustomer = functions.https.onCall(async (data, context) => {
-  // エラーハンドリング
   if (!context.auth) {
     throw new functions.https.HttpsError(
       'permission-denied',
@@ -14,35 +16,31 @@ export const createCustomer = functions.https.onCall(async (data, context) => {
     );
   }
 
-  // Stripeに作成した顧客情報を受け取る
   const customer = await stripe.customers.create(data);
   console.log(customer);
   // Firestoreの顧客コレクションに顧客IDを記録
   return db.doc(`customers/${context.auth.uid}`).set({
     userId: context.auth.uid,
-    customerId: customer.id // 顧客のID
+    email: customer.email,
+    customerId: customer.id,
+    subscriptionId: ''
   });
 });
 
-//----------------------------------------------------
-// 環境変数から税率IDを取得
-const taxId = functions.config().stripe.tax_id;
-const planId = functions.config().stripe.plan_id;
-
-// 課金を開始する関数
+//----------------------課金を開始する関数------------------------------
 export const registerForBilling = functions.https.onCall(
   async (
     data: {
+      userId: string;
       customerId: string; // 課金するカスタマーのID
       planId: string; // 登録するプランのID
+      subscriptionId?: string;
     },
-    context
+    context: any
   ) => {
-    // 例外処理
     if (!context.auth) {
       throw new Error('認証エラー');
     }
-
     // 課金を開始し、課金情報を取得
     const subscription = await stripe.subscriptions.create({
       customer: data.customerId,
@@ -51,35 +49,51 @@ export const registerForBilling = functions.https.onCall(
     });
     console.log(subscription);
     // ユーザーに課金IDを保持させる
-    return db.doc(`customers/${context.auth.uid}`).update({
-      subscriptionId: subscription.items.plan
+    await db.doc(`customers/${context.auth.uid}`).update({
+      planId,
+      subscriptionId: subscription.id,
+      startedAt: new Date()
     });
   }
 );
 
-//---------------------------------------------------
-
-//-------課金停止処理
+//---------------課金停止処理--------------------
 
 export const withdrawForBilling = functions.https.onCall(
   async (
     data: {
-      customerId: string; //課金停止するカスタマーのID
-      planId: string; //課金停止するプランのID
+      userId: string;
+      customerId: string;
+      planId: string;
+      subscriptionId: string;
     },
     context
   ) => {
-    // 例外処理
     if (!context.auth) {
       throw new Error('認証エラー');
     }
-    const subscription = await stripe.subscriptions.del({
-      customer: data.customerId,
-      default_tax_rates: [taxId],
-      items: [{ plan: data.planId }]
-    });
-    return db.doc(`users/${context.auth.uid}`).update({
-      subscriptionId: subscription.id
+
+    const userPayment = (
+      await db.doc(`customers/${context.auth.uid}`).get()
+    ).data();
+
+    if (!userPayment) {
+      return;
+    }
+
+    await stripe.subscriptions.del(userPayment.subscriptionId);
+    await db.doc(`customers/${context.auth.uid}`).update({
+      subscriptionId: null,
+      planId: 'free'
     });
   }
 );
+
+// ----------------------ユーザー削除時の処理-------------------------
+
+export const deleteCustomer = functions.auth.user().onDelete(async user => {
+  // コレクションから顧客を取得
+  const customer: any = (await db.doc(`customers/${user.uid}`).get()).data();
+  // 顧客IDを使って顧客を削除
+  return stripe.customers.del(customer.customerId);
+});

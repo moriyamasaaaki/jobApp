aba266c30ed07d71d12bca39ead75515034c7707
moriyamasaaaki <moriya-7071@outlook.com>
2020-03-05 14:25:35 +0900

    feat(header): ログイン認証 #115

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-
+import { Observable } from 'rxjs';
+import { Status } from '../interfaces/status';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  uid: string;
+  userLoginStatus: boolean;
+  companyLoginStatus: boolean;
   user$ = this.authService.afUser$;
 
   constructor(private authService: AuthService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userLoginToggle();
+    this.comapnyLoginToggles();
+  }
+
+  userLoginToggle() {
+    if (this.authService.getUserLogin(this.authService.uid)) {
+      this.userLoginStatus = true;
+      this.companyLoginStatus = false;
+    } else if (!this.authService.getUserLogin(this.authService.uid)) {
+      this.userLoginStatus = false;
+      this.companyLoginStatus = true;
+    }
+  }
+  comapnyLoginToggles() {
+    if (this.authService.getCompanyLogin(this.authService.uid)) {
+      this.companyLoginStatus = true;
+      this.userLoginStatus = false;
+    } else if (!this.authService.getCompanyLogin(this.authService.uid)) {
+      this.companyLoginStatus = false;
+      this.userLoginStatus = true;
+    }
+  }
+
+  userLogin() {
+    this.authService.userLogin();
+    this.authService.getUserLogin(this.authService.uid);
+    this.userLoginStatus = true;
+    this.companyLoginStatus = false;
+  }
+
+  companyLogin() {
+    this.authService.companyLogin();
+    this.authService.getCompanyLogin(this.authService.uid);
+    this.companyLoginStatus = true;
+    this.userLoginStatus = false;
+  }
 
   logout() {
-    this.authService.logout();
+    this.authService.logout(this.authService.uid);
+    this.userLoginStatus = false;
+    this.companyLoginStatus = false;
   }
 }

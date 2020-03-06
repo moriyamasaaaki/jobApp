6c74a4cac599e1670c64bf232d06b827d91e0da8
moriyamasaaaki <moriya-7071@outlook.com>
2020-03-06 16:51:36 +0900

    feat(search): 検索（Algolia)機能追加 #175

--- src/app/home/search/search.component.ts
+++ src/app/home/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { searchClient } from '../../../environments/environment';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -8,14 +9,34 @@ import { searchClient } from '../../../environments/environment';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  user$ = this.authService.afUser$;
+
+  inputParams = {
+    hitsPerPage: 10,
+    query: ''
+  };
   config = {
     indexName: 'JobPosts',
     searchClient
   };
 
-  user$ = this.authService.afUser$;
+  search(workPlace: string) {
+    this.router.navigate(['/search'], {
+      queryParams: {
+        workPlace
+      }
+    });
+  }
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.route.queryParamMap.subscribe(map => {
+      this.inputParams.query = map.get('q');
+    });
+  }
 
   ngOnInit() {}
 

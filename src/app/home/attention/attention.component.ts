850dd829cddf59bfe460a316a0656168f5d185b0
moriyamasaaaki <moriya-7071@outlook.com>
2020-03-28 05:12:14 +0900

    feat(attention): swiper追加 #275

--- src/app/home/attention/attention.component.ts
+++ src/app/home/attention/attention.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DetailJob } from 'src/app/interfaces/article';
 import { Observable } from 'rxjs';
 import { JobPostService } from 'src/app/services/job-post.service';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-attention',
@@ -9,6 +10,15 @@ import { JobPostService } from 'src/app/services/job-post.service';
   styleUrls: ['./attention.component.scss']
 })
 export class AttentionComponent implements OnInit {
+  config: SwiperConfigInterface = {
+    loop: true,
+    navigation: true,
+    pagination: true,
+    centeredSlides: true,
+    speed: 500,
+    slidesPerView: 4
+  };
+  selectedJobId = 0;
   jobs$: Observable<DetailJob[]> = this.jobPostService.getAttentionJobs();
 
   constructor(private jobPostService: JobPostService) {}

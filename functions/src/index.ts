7bc6bcae02e415eee930e3c88c74d06216d436f5
moriyamasaaaki <moriya-7071@outlook.com>
2020-04-17 14:57:32 +0900

    feat(functions): 応募メール送信 #233

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,3 +5,4 @@ export * from './send-email.function';
 export * from './user-delete.function';
 export * from './widhdraw-email.function';
 export * from './render.function';
+export * from './recuit-email.function';
--- functions/src/recuit-email.function.ts
+++ functions/src/recuit-email.function.ts
@@ -12,30 +12,39 @@ const API_KEY = functions.config().sendgrid.key;
 sgMail.setApiKey(API_KEY);
 console.log(API_KEY);
 
-//ユーザー登録のメール送信
-export const userSendEmail = functions.firestore
-  .document('userProfile/{userId}')
+// 応募フォーム //
+export const recuitSendEmail = functions.firestore
+  .document('JobPosts/{id}/recuit/{userId}')
   .onCreate(async (snap, context) => {
     const userId = context.params.userId;
+    const id = context.params.id;
     return db
-      .doc(`userProfile/${userId}`)
+      .doc(`JobPosts/${id}/recuit/${userId}`)
       .get()
       .then(doc => {
         const eventId = context.eventId;
         return shouldEventRun(eventId)
           .then((should: boolean) => {
             if (should) {
-              const user: any = doc.data();
-              console.log(user);
+              const recuitDate: any = doc.data();
+              console.log(recuitDate);
               const msg = {
-                to: user.email,
+                to: recuitDate.companyEmail,
                 from: {
                   email: 'moriya-7071@outlook.com',
                   name: 'Proxy Works'
                 },
-                templateId: 'd-fcbcd1da133d49a29f1f1602fc1972c2',
+                templateId: 'd-fec48e26c33549f79a6a5ae934f38dfe',
                 dynamicTemplateData: {
-                  name: user.name
+                  companyName: recuitDate.companyName,
+                  companyTitle: recuitDate.companyTitle,
+                  name: recuitDate.name,
+                  year: recuitDate.bday.year,
+                  month: recuitDate.bday.month,
+                  day: recuitDate.bday.day,
+                  gender: recuitDate.gender,
+                  tel: recuitDate.tel,
+                  email: recuitDate.email
                 }
               };
               console.log(sgMail);
@@ -45,39 +54,43 @@ export const userSendEmail = functions.firestore
               return;
             }
           })
-          .then(() => console.log('ユーザーにメールを送信しました!'))
+          .then(() => console.log('企業様に求人応募のメールを送信しました!'))
           .catch(err => console.log(err));
       });
   });
 
-// 企業の登録メール送信
-export const companySendEmail = functions.firestore
-  .document('companyProfile/{companyUserId}')
+// 応募完了メール //
+export const recuitCompletionEmail = functions.firestore
+  .document('JobPosts/{id}/recuit/{userId}')
   .onCreate(async (snap, context) => {
-    const companyUserId = context.params.companyUserId;
-
+    const userId = context.params.userId;
+    const id = context.params.id;
     return db
-      .doc(`companyProfile/${companyUserId}`)
+      .doc(`JobPosts/${id}/recuit/${userId}`)
       .get()
       .then(doc => {
         const eventId = context.eventId;
         return shouldEventRun(eventId)
           .then((should: boolean) => {
             if (should) {
-              const company: any = doc.data();
-              console.log(company);
+              const completeDate: any = doc.data();
+              console.log(completeDate);
               const msg = {
-                to: company.email,
+                to: completeDate.email,
                 from: {
                   email: 'moriya-7071@outlook.com',
                   name: 'Proxy Works'
                 },
-                templateId: 'd-b5af033ea4454189a9e902eace8d21ca',
+                templateId: 'd-1571473911944574b27173f8ef7d14de',
                 dynamicTemplateData: {
-                  name: company.name,
-                  lastName: company.lastName,
-                  firstName: company.firstName,
-                  password: company.password
+                  companyTitle: completeDate.companyTitle,
+                  name: completeDate.name,
+                  year: completeDate.bday.year,
+                  month: completeDate.bday.month,
+                  day: completeDate.bday.day,
+                  gender: completeDate.gender,
+                  tel: completeDate.tel,
+                  email: completeDate.email
                 }
               };
               console.log(sgMail);
@@ -87,7 +100,7 @@ export const companySendEmail = functions.firestore
               return;
             }
           })
-          .then(() => console.log('企業様にメールを送信しました!'))
+          .then(() => console.log('応募ありがとうございます！'))
           .catch(err => console.log(err));
       });
   });

613825eebb1dd2f8c0affe0ebc4acb9286459be9
moriyamasaaaki <moriya-7071@outlook.com>
2020-04-27 00:35:46 +0900

    feat(mypage): mat-tab追加 #374

--- src/app/mypage/mypage.module.ts
+++ src/app/mypage/mypage.module.ts
@@ -6,9 +6,15 @@ import { MypageComponent } from './mypage/mypage.component';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-
+import { MatTabsModule } from '@angular/material/tabs';
 @NgModule({
   declarations: [MypageComponent],
-  imports: [CommonModule, MypageRoutingModule, MatButtonModule, MatDialogModule]
+  imports: [
+    CommonModule,
+    MypageRoutingModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatTabsModule
+  ]
 })
 export class MypageModule {}
